efcbda30bc81047bf60665864a0c21a39a9d871d
Alex <alext@rtradetechnologies.com>
2019-07-04 10:48:53 -0700

    chore: revert GetBackedHas

--- datastore.go
+++ datastore.go
@@ -73,7 +73,11 @@ func (d *Datastore) Get(key datastore.Key) ([]byte, error) {
 
 // Has returns whether the key is present in our datastore
 func (d *Datastore) Has(key datastore.Key) (bool, error) {
-	return datastore.GetBackedHas(d, key)
+	data, err := d.Get(key)
+	if err != nil {
+		return false, err
+	}
+	return data != nil, nil
 }
 
 // GetSize returns the size of the value referenced by key
